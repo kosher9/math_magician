d548df120c345c524dd731dcfc74879bde3e5867
kosher <lionel.kossi@gmail.com>
2022-04-22 01:36:42 +0000

    Add event handler to all button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-Ctn">
-        <Calculator />
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.handleButtonClick = this.handleButtonClick.bind(this)
+    this.state = {text: '0'}
+  }
+
+  handleButtonClick(newText) {
+    if(this.state.text === '0' && newText !== '0') {
+      this.setState({text: newText})
+    } else {
+      this.setState({text: this.state.text + newText})
+    }
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-Ctn">
+          <Calculator output={this.state.text} handleButtonClick={this.handleButtonClick}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,6 +5,11 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleButtonClick = this.handleButtonClick.bind(this)
+  }
+
+  handleButtonClick(e) {
+    this.props.handleButtonClick(e.target.innerText)
   }
 
   render() {
@@ -12,41 +17,45 @@ class Calculator extends React.Component {
       <div className="Calculator">
         <div className="Calculator-Ctn">
           <div className="Calculator-Row">
-            <span className="output">0</span>
+            <span className="output">{this.props.output}</span>
           </div>
           <div className="Calculator-Row num-op">
-            <div className="btn"><span>AC</span></div>
-            <div className="btn"><span>+/-</span></div>
-            <div className="btn"><span>%</span></div>
-            <div className="btn orange"><span>÷</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>AC</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>+/-</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>%</span></div>
+            <div className="btn orange" onClick={this.handleButtonClick}><span>÷</span></div>
           </div>
           <div className="Calculator-Row num-op">
-            <div className="btn"><span>7</span></div>
-            <div className="btn"><span>8</span></div>
-            <div className="btn"><span>9</span></div>
-            <div className="btn orange"><span>x</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>7</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>8</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>9</span></div>
+            <div className="btn orange" onClick={this.handleButtonClick}><span>x</span></div>
           </div>
           <div className="Calculator-Row num-op">
-            <div className="btn"><span>4</span></div>
-            <div className="btn"><span>5</span></div>
-            <div className="btn"><span>6</span></div>
-            <div className="btn orange"><span>-</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>4</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>5</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>6</span></div>
+            <div className="btn orange" onClick={this.handleButtonClick}><span>-</span></div>
           </div>
           <div className="Calculator-Row num-op">
-            <div className="btn"><span>1</span></div>
-            <div className="btn"><span>2</span></div>
-            <div className="btn"><span>3</span></div>
-            <div className="btn orange"><span>+</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>1</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>2</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>3</span></div>
+            <div className="btn orange" onClick={this.handleButtonClick}><span>+</span></div>
           </div>
           <div className="Calculator-Row num-op">
-            <div className="btn"><span>0</span></div>
-            <div className="btn"><span>.</span></div>
-            <div className="btn orange"><span>=</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>0</span></div>
+            <div className="btn" onClick={this.handleButtonClick}><span>.</span></div>
+            <div className="btn orange" onClick={this.handleButtonClick}><span>=</span></div>
           </div>
         </div>
       </div>
     );
   }
+}
+
+Calculator.defaultProps = {
+  output: '0'
 }
 
 export default Calculator;
