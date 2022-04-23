368ddeb1998d886a9af48693749b4d0d9afeebaa
kosher <lionel.kossi@gmail.com>
2022-04-23 02:45:03 +0000

    Refactor using hooks

--- src/App.js
+++ src/App.js
@@ -2,45 +2,14 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-    this.state = { text: '0' };
-  }
-
-  handleButtonClick({ x, y }) {
-    const { text } = this.state;
-    if (x === 'clear') {
-      this.setState({ text: y });
-      return;
-    }
-    if (x === 'old') {
-      this.setState({ text: y });
-      return;
-    }
-    if (text === '0' && y === '0') {
-      this.setState({ text: y });
-    } else if (y !== '0' && text === '0') {
-      this.setState({ text: y });
-    } else {
-      this.setState({ text: text + y });
-    }
-  }
-
-  render() {
-    const { text } = this.state;
-    return (
-      <div className="App">
-        <div className="App-Ctn">
-          <Calculator
-            output={text}
-            handleButtonClick={this.handleButtonClick}
-          />
-        </div>
+function App() {
+  return (
+    <div className="App">
+      <div className="App-Ctn">
+        <Calculator />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,260 +1,241 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import './Calculator.css';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-  }
+function Calculator() {
+  const [data, setData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleButtonClick(e) {
-    const handleButton = { x: '', y: '' };
-    handleButton.x = '';
-    handleButton.y = e.target.innerText;
-    let { total, next, operation } = this.state;
-    const data = calculate({ total, next, operation }, e.target.innerText);
-    total = data.total;
-    next = data.next;
-    operation = data.operation;
-    this.setState({ total, next, operation });
+  const [output, setOutput] = useState('0');
 
-    const { handleButtonClick } = this.props;
-    if (total === null && next === null && operation === null) {
-      const x = 'clear';
-      const y = '0';
-      handleButtonClick({ x, y });
+  function handleButtonClick(e) {
+    const result = calculate(data, e.target.innerText);
+    setData(result);
+    const a = e.target.innerText;
+    if (a === 'AC') {
+      setOutput('0');
+      return;
+    }
+    const { total, next, operation } = result;
+    if (total === null && next !== null && output === '0') {
+      setOutput(next);
     } else if (total !== null && next === null && operation === null) {
-      const x = 'clear';
-      const y = total;
-      handleButtonClick({ x, y });
-    } else if (total === null && next && next.length === 1 && operation === undefined) {
-      const x = 'old';
-      const y = next;
-      handleButtonClick({ x, y });
+      setOutput(total);
     } else {
-      const x = '';
-      const y = e.target.innerText;
-      handleButtonClick({ x, y });
+      setOutput(output + a);
     }
   }
 
-  render() {
-    const { output } = this.props;
-    return (
-      <div className="Calculator">
-        <div className="Calculator-Ctn">
-          <div className="Calculator-Row">
-            <span className="output">{output}</span>
+  return (
+    <div className="Calculator">
+      <div className="Calculator-Ctn">
+        <div className="Calculator-Row">
+          <span className="output">{output}</span>
+        </div>
+        <div className="Calculator-Row num-op">
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>AC</span>
           </div>
-          <div className="Calculator-Row num-op">
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>AC</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>+/-</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>%</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn orange"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>÷</span>
-            </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>+/-</span>
           </div>
-          <div className="Calculator-Row num-op">
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>7</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>8</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>9</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn orange"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>x</span>
-            </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>%</span>
           </div>
-          <div className="Calculator-Row num-op">
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>4</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>5</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>6</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn orange"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>-</span>
-            </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn orange"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>÷</span>
           </div>
-          <div className="Calculator-Row num-op">
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>1</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>2</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>3</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn orange"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>+</span>
-            </div>
+        </div>
+        <div className="Calculator-Row num-op">
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>7</span>
           </div>
-          <div className="Calculator-Row num-op">
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>0</span>
-            </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="btn"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>.</span>
-            </div>
-            <div
-              fo
-              role="button"
-              tabIndex={0}
-              className="btn orange"
-              onClick={this.handleButtonClick}
-              onKeyDown={this.handleButtonClick}
-            >
-              <span>=</span>
-            </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>8</span>
+          </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>9</span>
+          </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn orange"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>x</span>
+          </div>
+        </div>
+        <div className="Calculator-Row num-op">
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>4</span>
+          </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>5</span>
+          </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>6</span>
+          </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn orange"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>-</span>
+          </div>
+        </div>
+        <div className="Calculator-Row num-op">
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>1</span>
+          </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>2</span>
+          </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>3</span>
+          </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn orange"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>+</span>
+          </div>
+        </div>
+        <div className="Calculator-Row num-op">
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>0</span>
+          </div>
+          <div
+            role="button"
+            tabIndex={0}
+            className="btn"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>.</span>
+          </div>
+          <div
+            fo
+            role="button"
+            tabIndex={0}
+            className="btn orange"
+            onClick={handleButtonClick}
+            onKeyDown={handleButtonClick}
+          >
+            <span>=</span>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-Calculator.defaultProps = {
+/* Calculator.defaultProps = {
   output: '0',
   handleButtonClick: {
     x: '',
     y: '',
   },
-};
+}; */
 
-Calculator.propTypes = {
+/* Calculator.propTypes = {
   output: PropTypes.string,
   handleButtonClick: {
     x: PropTypes.string,
     y: PropTypes.string,
   }.PropTypes,
-};
+}; */
 
 export default Calculator;
